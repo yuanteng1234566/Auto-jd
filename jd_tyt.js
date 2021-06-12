5e10f3b1d7c80c6fd6b4e0a48c92653554602195
asd920 <82523418+asd920@users.noreply.github.com>
2021-06-12 17:00:13 +0000

    Update jd_tyt.js

--- jd_tyt.js
+++ jd_tyt.js
@@ -12,7 +12,7 @@ const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
 
 //IOS等用户直接用NobyDa的jd cookie
 let cookiesArr = [], cookie = '', message;
-let tytpacketId = '5d706efcf0464cac8b1deccad28434b6-amRfNDY4NWIyMTU3Zjg3NA!!';
+let tytpacketId = 'a73780009171495fa7e584774f0ee627-amRfNDY4NWIyMTU3Zjg3NA!!';
 // if (process.env.tytpacketId) {
 //   tytpacketId = process.env.tytpacketId;
 // }
