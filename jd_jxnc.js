8b171146c2c8dc43b7fcac45864a2634d984119c
asd920 <82523418+asd920@users.noreply.github.com>
2021-06-12 11:18:53 +0000

    Update jd_jxnc.js

--- jd_jxnc.js
+++ jd_jxnc.js
@@ -38,7 +38,7 @@ let tokenArr = []; // 用户 token 数组
 let currentToken = {}; // 当前用户 token
 let shareCode = ''; // 内置助力码
 let jxncShareCodeArr = []; // 用户 助力码 数组
-let currentShareCode = [{\"smp\":\"04a76ac1a5d541f44edc786361174c14\",\"active\":\"jdnc_1_3yuanshousibang210601_2\",\"joinnum\":\"1\"}]; // 当前用户 要助力的助力码
+let currentShareCode = ['{\"smp\":\"04a76ac1a5d541f44edc786361174c14\",\"active\":\"jdnc_1_3yuanshousibang210601_2\",\"joinnum\":\"1\"}']; // 当前用户 要助力的助力码
 const openUrl = `openjd://virtual?params=${encodeURIComponent('{ "category": "jump", "des": "m", "url": "https://wqsh.jd.com/sns/201912/12/jxnc/detail.html?ptag=7155.9.32&smp=b47f4790d7b2a024e75279f55f6249b9&active=jdnc_1_chelizi1205_2"}',)}`; // 打开京喜农场
 let subTitle = '', message = '', option = {'open-url': openUrl}; // 消息副标题，消息正文，消息扩展参数
 const JXNC_API_HOST = 'https://wq.jd.com/';
