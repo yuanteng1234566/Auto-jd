fe774aab796eefbb964162e6df9bc6683d14e744
asd920 <82523418+asd920@users.noreply.github.com>
2021-06-13 06:15:10 +0000

    Update jdPlantBeanShareCodes.js

--- jdPlantBeanShareCodes.js
+++ jdPlantBeanShareCodes.js
@@ -8,9 +8,9 @@
 // 如: 京东账号1的shareCode1@京东账号1的shareCode2&京东账号2的shareCode1@京东账号2的shareCode2
 let PlantBeanShareCodes = [
   //账号一的好友shareCode,不同好友的shareCode中间用@符号隔开
-  'e7lhibzb3zek2fjsi7wcketyu2lpa5d4a5mxig@4npkonnsy7xi2jkey7eknaeqvl55a5qkubun6cy',
+  'e7lhibzb3zek2fjsi7wcketyu2lpa5d4a5mxiga@4npkonnsy7xi2jkey7eknaeqvl55a5qkubun6cy',
   //账号二的好友shareCode,不同好友的shareCode中间用@符号隔开
-  'e7lhibzb3zek2fjsi7wcketyu2lpa5d4a5mxig@4npkonnsy7xi2jkey7eknaeqvl55a5qkubun6cy',
+  'e7lhibzb3zek2fjsi7wcketyu2lpa5d4a5mxiga@4npkonnsy7xi2jkey7eknaeqvl55a5qkubun6cy',
 ]
 // 判断github action里面是否有种豆得豆互助码
 if (process.env.PLANT_BEAN_SHARECODES) {
