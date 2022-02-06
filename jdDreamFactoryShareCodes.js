eb9d87803ae604f3bffc3983ffb23410befd51d3
setsailqq <44343422+setsailqq@users.noreply.github.com>
2022-02-06 11:30:36 +0800

    Update jdDreamFactoryShareCodes.js

--- jdDreamFactoryShareCodes.js
+++ jdDreamFactoryShareCodes.js
@@ -7,8 +7,8 @@
 // 同一个京东账号的好友互助码用@符号隔开,不同京东账号之间用&符号或者换行隔开,下面给一个示例
 // 如: 京东账号1的shareCode1@京东账号1的shareCode2&京东账号2的shareCode1@京东账号2的shareCode2
 let shareCodes = [
-  'zulT8XTLky1DEZdgXef6OA==@6lw84c1ARwpoRyOtfnF77g==',//账号一的好友shareCode,不同好友中间用@符号隔开
-  'zulT8XTLky1DEZdgXef6OA==@6lw84c1ARwpoRyOtfnF77g==',//账号二的好友shareCode，不同好友中间用@符号隔开
+  '',//账号一的好友shareCode,不同好友中间用@符号隔开
+  '',//账号二的好友shareCode，不同好友中间用@符号隔开
 ]
 // 判断github action里面是否有京喜工厂互助码
 if (process.env.DREAM_FACTORY_SHARE_CODES) {
