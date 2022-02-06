930638137a958361278670dad9b88124ebe7f481
setsailqq <44343422+setsailqq@users.noreply.github.com>
2022-02-06 12:24:20 +0800

    Update jd_fruit.js

--- jd_fruit.js
+++ jd_fruit.js
@@ -30,11 +30,11 @@ let cookiesArr = [], cookie = '', jdFruitShareArr = [], isBox = false, notify, n
 //下面给出两个账号的填写示例（iOS只支持2个京东账号）
 let shareCodes = [ // 这个列表填入你要助力的好友的shareCode
   //账号一的好友shareCode,不同好友的shareCode中间用@符号隔开
- 'd4f5c457f7f74ebb8d8995f6ecc2a8ab@52484bd8bbdd48cca85ff09a983a762d@643662ac768b413fb6d7f6ee6a675614',
+  '67b45c2bb097402d93ee4e8ce7b3cc49@c38299e862174bdaa2d525faa5ea7059@2074936ceb8942cd93dcd3b9b49e4949',
  //账号二的好友shareCode,不同好友的shareCode中间用@符号隔开
- 'd4f5c457f7f74ebb8d8995f6ecc2a8ab@52484bd8bbdd48cca85ff09a983a762d@643662ac768b413fb6d7f6ee6a675614',
+'f8e4cb7f0abb44059c2263bef783347f@c38299e862174bdaa2d525faa5ea7059@67b45c2bb097402d93ee4e8ce7b3cc49',
  //账号三的好友shareCode,不同好友的shareCode中间用@符号隔开
- 'd4f5c457f7f74ebb8d8995f6ecc2a8ab@52484bd8bbdd48cca85ff09a983a762d@643662ac768b413fb6d7f6ee6a675614',
+'f8e4cb7f0abb44059c2263bef783347f@56e40ef4ead440b4a7b9175fabdb437d@67b45c2bb097402d93ee4e8ce7b3cc49',
 ]
 let message = '', subTitle = '', option = {}, isFruitFinished = false;
 const retainWater = 100;//保留水滴大于多少g,默认100g;
