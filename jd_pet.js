14cb0cf710a12f956060af408204f95d296fbc76
setsailqq <44343422+setsailqq@users.noreply.github.com>
2022-02-06 12:37:13 +0800

    Update jd_pet.js

--- jd_pet.js
+++ jd_pet.js
@@ -31,9 +31,9 @@ let cookiesArr = [], cookie = '', jdPetShareArr = [], isBox = false, notify,newS
 //下面给出两个账号的填写示例（iOS只支持2个京东账号）
 let shareCodes = [ // IOS本地脚本用户这个列表填入你要助力的好友的shareCode
    //账号一的好友shareCode,不同好友的shareCode中间用@符号隔开
-  'MTAxODcxOTI2NTAwMDAwMDAxMDk2NTk2OQ==@MTEyOTEzNzMzMDAwMDAwMDUwMjEyNjA3@MTAxODc2NTEzNTAwMDAwMDAyMDUxNjkzMw==',
-  //账号二的好友shareCode,不同好友的shareCode中间用@符号隔开
-  'MTAxODcxOTI2NTAwMDAwMDAxMDk2NTk2OQ==@MTEyOTEzNzMzMDAwMDAwMDUwMjEyNjA3@MTAxODc2NTEzNTAwMDAwMDAyMDUxNjkzMw==',
+  'MTAxODc2NTEzMzAwMDAwMDAzMjgxNTgyNQ==@MTAxODc2NTEzMzAwMDAwMDAyOTgzNDM4Mw==@MTAxODc2NTEzMzAwMDAwMDAyOTgzNDM4MQ=='
+'MTAxODc2NTEzMzAwMDAwMDAyOTgzNDM4Mw==@MTAxODc2NTEzMzAwMDAwMDAyOTgzNDM4MQ==@MTAxODcxOTI2NTAwMDAwMDAwNDY2MDAwOQ=='
+'MTAxODc2NTEzMzAwMDAwMDAyOTgzNDM4MQ==@MTAxODcxOTI2NTAwMDAwMDAwNDY2MDAwOQ==@MTE1NDQ5OTIwMDAwMDAwMzY4NjU2MjE='
 ]
 let message = '', subTitle = '', option = {};
 let jdNotify = false;//是否关闭通知，false打开通知推送，true关闭通知推送
