c3c09d2b4e8be0a1fd5294b445dcfdc16b94752e
asd920 <82523418+asd920@users.noreply.github.com>
2022-02-07 10:23:26 +0800

    Update jd_speed_redpocke.js

--- jd_speed_redpocke.js
+++ jd_speed_redpocke.js
@@ -394,11 +394,7 @@ function cashOut(id,poolBaseId,prizeGroupId,prizeBaseId,) {
 }
 
 function invite2() {
-  let inviterIdArr = [
-    "5V7vHE23qh2EkdBHXRFDuA==",
-    "Yvk/fMWJC/6lvcx1iUFnsw==",
-    "E9EvSFNuA1pahSQT0uSsXkW1v0j+QOHQbk8+peJYc0I="
-  ]
+  let inviterIdArr = []
   let inviterId = inviterIdArr[Math.floor((Math.random() * inviterIdArr.length))]
   let options = {
     url: "https://api.m.jd.com/",
